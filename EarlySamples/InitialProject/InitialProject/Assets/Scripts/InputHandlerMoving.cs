f5db7f27a75755f076bb6cfe1e0e31a06cd8b563
HKommaraju <48811258+HKommaraju@users.noreply.github.com>
2021-04-11 20:11:25 -0400

    CubeTest movements and quit requests implemented

---
+++ EarlySamples/InitialProject/InitialProject/Assets/Scripts/InputHandlerMoving.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace OhioState.RoadCrossing
+{
+    public class InputHandlerMoving : MonoBehaviour
+    {
+        public InputActionAsset testActions;
+        InputActionMap keyboardActionMap;
+        InputAction jumpInputAction;
+        InputAction moveLeftAction;
+        InputAction moveRightAction;
+        InputAction moveUpAction;
+        InputAction moveDownAction;
+        Rigidbody player;
+        Vector3 movementVector;
+        [SerializeField]private float impulseForce = 100;
+        bool moveImplemented;
+
+        //[SerializeField] EventStarTrekBeamDown spawner;
+
+        private void Awake()
+        {
+            keyboardActionMap = testActions.FindActionMap("Player");
+            moveLeftAction = keyboardActionMap.FindAction("MoveLeft");
+            moveRightAction = keyboardActionMap.FindAction("MoveRight");
+            moveUpAction = keyboardActionMap.FindAction("MoveUp");
+            moveDownAction = keyboardActionMap.FindAction("MoveDown");
+            jumpInputAction = keyboardActionMap.FindAction("Jump");
+
+            jumpInputAction.performed += HandleJumpInputEvent;
+            moveRightAction.performed += HandleMoveRightEvent;
+            moveLeftAction.performed += HandleMoveLeftEvent;
+            moveUpAction.performed += HandleMoveUpEvent;
+            moveDownAction.performed += HandleMoveDownEvent;
+
+            movementVector = Vector3.zero;
+            player = GetComponent<Rigidbody>();
+            moveImplemented = true;
+            
+        }
+
+        private void HandleMoveDownEvent(InputAction.CallbackContext obj)
+        {
+            movementVector = new Vector3(0, 0, -1*impulseForce);
+            moveImplemented = false;
+        }
+
+        private void HandleMoveUpEvent(InputAction.CallbackContext obj)
+        {
+            movementVector = new Vector3(0, 0, impulseForce);
+            moveImplemented = false;
+        }
+
+        private void HandleMoveLeftEvent(InputAction.CallbackContext obj)
+        {
+            movementVector = new Vector3(-1*impulseForce, 0, 0);
+            moveImplemented = false;
+        }
+
+        private void HandleMoveRightEvent(InputAction.CallbackContext obj)
+        {
+            movementVector = new Vector3(impulseForce, 0, 0);
+            moveImplemented = false;
+        }
+
+        private void HandleJumpInputEvent(InputAction.CallbackContext obj)
+        {
+            movementVector = new Vector3(0, impulseForce, 0);
+            moveImplemented = false;
+        }
+
+        private void FixedUpdate()
+        {
+            if(!moveImplemented)
+            {
+                moveImplemented = true;
+                player.AddForce(movementVector, ForceMode.Force);
+            }
+        }
+
+    }
+}
+
+
+
--- EarlySamples/InitialProject/InitialProject/Assets/Scripts/QuitHandler.cs
+++ EarlySamples/InitialProject/InitialProject/Assets/Scripts/QuitHandler.cs
@@ -1,31 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace OhioState.RoadCrossing
 {
     //The class to handle the event "Quit"
     public class QuitHandler : MonoBehaviour
     {
+        public InputActionAsset testActions;
+        InputActionMap UIActionMap;
+        InputAction quitInputAction;
+
         private void Start()
         {
-            DummyTestGameManager QuitHandler = DummyTestGameManager.Instance; // Get the instance of DummyTestGameManager 
-            QuitHandler.onQuit += QuitHandler_Onquit; // Subscribe to the OnQuit event from the instance
+            UIActionMap = testActions.FindActionMap("UI");
+            quitInputAction = UIActionMap.FindAction("Quit");
+
+            quitInputAction = UIActionMap.FindAction("Quit");
+            quitInputAction.performed += HandleQuitRequest;
+
         }
 
-        //Hnadles the Quit event.
-        private void QuitHandler_Onquit(object sender, System.EventArgs e)
+        private void HandleQuitRequest(InputAction.CallbackContext obj)
         {
             //Based on platform, either closes the editor, or the entire application(if fully built)
             #if UNITY_EDITOR
-            
-                UnityEditor.EditorApplication.isPlaying = false;
+
+            UnityEditor.EditorApplication.isPlaying = false;
 
             #else
                 Application.Quit();
 
             #endif
-         }   
-        
+        }
+
+      
+       
     }
 }
